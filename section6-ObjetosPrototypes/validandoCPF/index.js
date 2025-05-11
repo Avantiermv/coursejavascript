d61b38f2e9fe7b1730a3206f668e33c68ad86695
Marcos Vinicius <avantiermv@gmail.com>
2025-05-11 16:01:49 -0300

    .

--- section6-ObjetosPrototypes/validandoCPF/index.js
+++ section6-ObjetosPrototypes/validandoCPF/index.js
@@ -1,3 +1,27 @@
-const cpfDoUsuario = document.getElementById('cpf');
-let cpfLimpo = cpfDoUsuario.value.replace(/D+/g, '');
+function ValidarCPF(cpfEnviado){
+        Object.defineProperty(this, 'cpfLimpo',{
+            enumerable: true,
+            get: function (){
+                return cpfEnviado.replace(/\D+/g, '');
+            }
+        });
+}
 
+ValidarCPF.prototype.validarAqui = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criarDigito(cpfParcial);
+    const digito2 = '';
+
+    return true;
+}
+
+ValidarCPF.prototype.criarDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+    console.log(cpfArray);
+}
+
+const cpf = new ValidarCPF('705.484.450-52');
+console.log(cpf.validarAqui());
