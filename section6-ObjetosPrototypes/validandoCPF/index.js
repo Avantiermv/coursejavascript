4bba0b4f786be682051d74eb3b1a0adfd4a53cc0
Marcos Vinicius <avantiermv@gmail.com>
2025-05-19 13:59:09 -0300

    .

--- section6-ObjetosPrototypes/validandoCPF/index.js
+++ section6-ObjetosPrototypes/validandoCPF/index.js
@@ -10,18 +10,35 @@ function ValidarCPF(cpfEnviado){
 ValidarCPF.prototype.validarAqui = function() {
     if(typeof this.cpfLimpo === 'undefined') return false;
     if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
     
     const cpfParcial = this.cpfLimpo.slice(0, -2);
     const digito1 = this.criarDigito(cpfParcial);
-    const digito2 = '';
+    const digito2 = this.criarDigito(cpfParcial + digito1);
 
-    return true;
+    const cpfValidado = cpfParcial + digito1 + digito2;
+    return cpfValidado === this.cpfLimpo;
 }
 
 ValidarCPF.prototype.criarDigito = function(cpfParcial) {
     const cpfArray = Array.from(cpfParcial);
-    console.log(cpfArray);
+   
+    let regr = cpfArray.length + 1;
+    const somaDoCPF = cpfArray.reduce((ac, valor) => {
+        ac += (regr * Number(valor));
+        regr--;
+        return ac;
+    }, 0);
+
+    let digito = 11 - (somaDoCPF % 11);
+    return digito > 9 ? '0' : String(digito);
+    
 }
 
-const cpf = new ValidarCPF('705.484.450-52');
+ValidarCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+}
+
+const cpf = new ValidarCPF('111.111.111-11');
 console.log(cpf.validarAqui());
