9b5c433c80b518224a120e202a2ebdfd969d9076
Marcos Vinicius <avantiermv@gmail.com>
2025-07-14 16:49:33 -0300

    .

---
+++ section9-javaScriptTooling-ES6Modules/modelo2/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var nome = "marcos";
+alert(nome);
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
