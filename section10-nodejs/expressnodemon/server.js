7ace6f7f26f3708f1a8aeb4e4bebd1e7aa122ec6
Marcos Vinicius <avantiermv@gmail.com>
2025-08-11 17:17:39 -0300

    .

--- section10-nodejs/expressnodemon/server.js
+++ section10-nodejs/expressnodemon/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send("Rapaz, el tá sem zap");
+    response.send("Acho que nã");
 });
 
 app.listen(3000, () => {
     console.log("Acessar: http://localhost:3000");
-    console.log("Servidor Está Executando na porta: 3000");
+    console.log("Servidor Executando na porta: 3000");
 });
